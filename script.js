5853801526c94af915dfe704bcf66489fbd59f4e
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-18 06:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const input = document.getElementById('fname');
+input.addEventListener('blur', () => {
+	let str = input.value.toUpperCase();
+	input.value = str;
+})
